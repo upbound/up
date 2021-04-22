17765a570696c74dce9ee6b749c8cd82dac16b55
hasheddan <georgedanielmangum@gmail.com>
2021-04-22 12:49:23 -0500

    Add controlplane create command
    up cloud controlplane create allows a user to create a new hosted
    control plane on Upbound cloud.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

---
+++ cmd/up/cloud/controlplane/create.go
@@ -0,0 +1,27 @@
+package controlplane
+
+import (
+	"context"
+
+	"github.com/alecthomas/kong"
+
+	cp "github.com/upbound/up-sdk-go/service/controlplanes"
+	"github.com/upbound/up/internal/cloud"
+)
+
+// CreateCmd creates a hosted control plane on Upbound Cloud.
+type CreateCmd struct {
+	Name string ` arg:"" required:"" help:"Name of control plane."`
+
+	Description string `short:"d" help:"Description for control plane."`
+}
+
+// Run executes the create command.
+func (c *CreateCmd) Run(kong *kong.Context, client *cp.Client, cloudCtx *cloud.Context) error {
+	_, err := client.Create(context.Background(), &cp.ControlPlaneCreateParameters{
+		Namespace:   cloudCtx.Org,
+		Name:        c.Name,
+		Description: c.Description,
+	})
+	return err
+}
