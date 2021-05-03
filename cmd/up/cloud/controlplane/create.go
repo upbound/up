6b6a419f5128a72b257af421bf6bfdb0524e6b3c
hasheddan <georgedanielmangum@gmail.com>
2021-05-03 13:40:22 -0500

    Use accounts instead of namespaces
    Updates to use account in parameters for control planes where namespace
    used to be used.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- cmd/up/cloud/controlplane/create.go
+++ cmd/up/cloud/controlplane/create.go
@@ -19,7 +19,7 @@ type CreateCmd struct {
 // Run executes the create command.
 func (c *CreateCmd) Run(kong *kong.Context, client *cp.Client, cloudCtx *cloud.Context) error {
 	_, err := client.Create(context.Background(), &cp.ControlPlaneCreateParameters{
-		Namespace:   cloudCtx.Org,
+		Account:     cloudCtx.Org,
 		Name:        c.Name,
 		Description: c.Description,
 	})
