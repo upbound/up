8eb5d5d22169c3175017c6bb716e9eb4cfab9261
hasheddan <georgedanielmangum@gmail.com>
2021-09-09 12:34:46 -0400

    Add up enterprise uninstall
    up enterprise uninstall uninstalls the enterprise chart from the
    currently pointed to cluster.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

---
+++ cmd/up/enterprise/uninstall.go
@@ -0,0 +1,47 @@
+// Copyright 2021 Upbound Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package enterprise
+
+import (
+	"net/url"
+
+	"github.com/upbound/up/internal/install"
+	"github.com/upbound/up/internal/install/helm"
+)
+
+// AfterApply sets default values in command after assignment and validation.
+func (c *uninstallCmd) AfterApply(insCtx *install.Context) error {
+	// NOTE(hasheddan): we do not specify credentials or that chart is from an
+	// OCI image as they are inconsequential at uninstall time.
+	mgr, err := helm.NewManager(insCtx.Kubeconfig,
+		enterpriseChart,
+		&url.URL{},
+		helm.WithNamespace(insCtx.Namespace))
+	if err != nil {
+		return err
+	}
+	c.mgr = mgr
+	return nil
+}
+
+// uninstallCmd uninstalls enterprise.
+type uninstallCmd struct {
+	mgr install.Manager
+}
+
+// Run executes the uninstall command.
+func (c *uninstallCmd) Run(insCtx *install.Context) error {
+	return c.mgr.Uninstall()
+}
