ad517199f4fb6c935d0961947ee5eeb80b4651b3
hasheddan <georgedanielmangum@gmail.com>
2021-11-09 09:00:36 -0500

    Add initial workspace implemenation
    Adds an initial workspace implementation that contains a cache of nodes
    and validators. Workspace parsing can either be done at the workspace or
    file level. In the future we will want to support incremental parsing
    using text change notifications from the LSP client. We will also want
    to support reparsing nodes based on changes to nodes they depend on.
    This can be accomplished by constructing a graph of all nodes (objects)
    in a workspace, then traversing the edges to a node when it is updated.
    
    Validators are currently constructured from the package cache, but
    assume the on-disk representation is CRDs, rather than xpkg tarballs.
    This will need to be adjusted. There is also currently only support for
    v1 CRDs, but we will need to support both v1beta1 CRDs and all versions
    of XRDs for validation schemas.
    
    A workspace is thread-safe, but currently use coarse-grained locking,
    which can likely be optimized in the future.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- go.mod
+++ go.mod
@@ -6,22 +6,27 @@ require (
 	github.com/Masterminds/semver v1.5.0
 	github.com/Masterminds/semver/v3 v3.1.1
 	github.com/alecthomas/kong v0.2.17
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/crossplane/crossplane v1.5.0
-	github.com/crossplane/crossplane-runtime v0.15.1-0.20210913015452-6a7a44ac50aa
+	github.com/crossplane/crossplane-runtime v0.15.1-0.20210930095326-d5661210733b
 	github.com/docker/docker v20.10.7+incompatible
-	github.com/ghodss/yaml v1.0.0
+	github.com/goccy/go-yaml v1.9.4
 	github.com/golang-jwt/jwt v3.2.1+incompatible
 	github.com/google/addlicense v0.0.0-20210428195630-6d92264d7170
 	github.com/google/go-cmp v0.5.6
 	github.com/google/go-containerregistry v0.6.0
 	github.com/google/uuid v1.2.0
 	github.com/goreleaser/nfpm/v2 v2.5.1
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/sourcegraph/go-lsp v0.0.0-20200429204803-219e11d77f5d
 	github.com/sourcegraph/jsonrpc2 v0.1.0
 	github.com/spf13/afero v1.6.0
 	github.com/spf13/cobra v1.2.1
 	github.com/upbound/up-sdk-go v0.1.0
+	go.starlark.net v0.0.0-20211013185944-b0039bd2cfe3 // indirect
+	golang.org/x/sys v0.0.0-20211103184734-ae416a5f93c7 // indirect
 	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d
 	helm.sh/helm/v3 v3.7.0
 	k8s.io/api v0.22.1
@@ -29,12 +34,15 @@ require (
 	k8s.io/apimachinery v0.22.1
 	k8s.io/cli-runtime v0.22.1
 	k8s.io/client-go v0.22.1
+	k8s.io/kube-openapi v0.0.0-20210421082810-95288971da7e
 	k8s.io/kubectl v0.22.1
 	sigs.k8s.io/controller-runtime v0.9.6
 	sigs.k8s.io/yaml v1.2.0
 )
 
 replace (
+	github.com/crossplane/crossplane-runtime => ../../crossplane/crossplane-runtime
 	github.com/docker/distribution => github.com/docker/distribution v0.0.0-20191216044856-a8371794149d
 	github.com/docker/docker => github.com/moby/moby v17.12.0-ce-rc1.0.20200618181300-9dc6525e6118+incompatible
+	k8s.io/kube-openapi => ../../../k8s.io/kube-openapi
 )
---
+++ internal/xpls/workspace.go
@@ -0,0 +1,377 @@
+// Copyright 2021 Upbound Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package xpls
+
+import (
+	"bufio"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"github.com/crossplane/crossplane-runtime/pkg/errors"
+	"github.com/goccy/go-yaml"
+	"github.com/goccy/go-yaml/ast"
+	"github.com/goccy/go-yaml/parser"
+	"github.com/sourcegraph/go-lsp"
+	ext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
+	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apiextensions-apiserver/pkg/apiserver/validation"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	apimachyaml "k8s.io/apimachinery/pkg/util/yaml"
+	verrors "k8s.io/kube-openapi/pkg/validation/errors"
+	"k8s.io/kube-openapi/pkg/validation/validate"
+	k8syaml "sigs.k8s.io/yaml"
+)
+
+// paths to extract GVK and name from objects that conform to Kubernetes
+// standard.
+var (
+	gvPath *yaml.Path
+	kPath  *yaml.Path
+	nPath  *yaml.Path
+)
+
+// builds static YAML path strings ahead of usage.
+func init() {
+	var err error
+	gvPath, err = yaml.PathString("$.apiVersion")
+	if err != nil {
+		panic(err)
+	}
+	kPath, err = yaml.PathString("$.kind")
+	if err != nil {
+		panic(err)
+	}
+	nPath, err = yaml.PathString("$.metadata.name")
+	if err != nil {
+		panic(err)
+	}
+}
+
+const (
+	errMissingValidatorFmt = "could not find validator for node: %s"
+	errParseNode           = "failed to parse node"
+)
+
+// A PackageNode represents a concrete node in an xpkg.
+// TODO(hasheddan): PackageNode should be refactored into separate
+// implementations for each node type (e.g. XRD, Composition, CRD, etc.).
+type PackageNode struct {
+	ast      ast.Node
+	nested   []ast.Node
+	fileName string
+	gvk      schema.GroupVersionKind
+}
+
+// GetAST gets the YAML AST node for this package node.
+func (p *PackageNode) GetAST() ast.Node {
+	return p.ast
+}
+
+// GetFileName gets the name of the file for this node.
+func (p *PackageNode) GetFileName() string {
+	return p.fileName
+}
+
+// GetDependants gets the set of nodes dependant on this node.
+// TODO(hasheddan): this method signature may change depending on how we want to
+// construct the node graph for a workspace.
+func (p *PackageNode) GetDependants() []NodeIdentifier {
+	return nil
+}
+
+// GetGVK returns the GroupVersionKind of this node.
+func (p *PackageNode) GetGVK() schema.GroupVersionKind {
+	return p.gvk
+}
+
+// GetNested returns the YAML AST nodes for all nested objects in a package
+// node.
+func (p *PackageNode) GetNested() []ast.Node {
+	return p.nested
+}
+
+// NodeIdentifier is the unique identifier of a node in a workspace.
+type NodeIdentifier struct {
+	name string
+	gvk  schema.GroupVersionKind
+}
+
+// nodeID constructs a NodeIdentifier from name and GVK.
+func nodeID(name string, gvk schema.GroupVersionKind) NodeIdentifier {
+	return NodeIdentifier{
+		name: name,
+		gvk:  gvk,
+	}
+}
+
+// A Node is a single object in the package workspace graph.
+type Node interface {
+	GetAST() ast.Node
+	GetFileName() string
+	GetDependants() []NodeIdentifier
+	GetGVK() schema.GroupVersionKind
+	GetNested() []ast.Node
+}
+
+// A Workspace represents a single xpkg workspace. It is safe for multi-threaded
+// use.
+type Workspace struct {
+	// The absolute path of the workspace.
+	root string
+
+	mu sync.RWMutex
+
+	// The node cache maintains a set of nodes present in a workspace. A node
+	// identifier is a combination of its GVK and name.
+	nodes map[NodeIdentifier]Node
+
+	// The validator cache maintains a set of validators loaded from the package cache.
+	validators map[schema.GroupVersionKind]*validate.SchemaValidator
+}
+
+// NewWorkspace constructs a new Workspace by loading validators from the
+// package cache. A workspace must be parsed before it can be validated.
+func NewWorkspace(root, cache string) (*Workspace, error) {
+	// TODO(hasheddan): currently we load all validators from the schema cache.
+	// In the future we will need to selectively and dynamically load validators
+	// based on changing dependencies in the crossplane.yaml.
+	vlds, err := validatorsFromDir(cache)
+	if err != nil {
+		return nil, err
+	}
+	return &Workspace{
+		root:       root,
+		nodes:      map[NodeIdentifier]Node{},
+		validators: vlds,
+	}, nil
+}
+
+// Parse parses all objects in a workspace and stores them in the node cache.
+func (w *Workspace) Parse() error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	return fs.WalkDir(os.DirFS(w.root), ".", func(p string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		fileName := filepath.Join(w.root, p)
+		return w.parseFile(fileName)
+	})
+}
+
+// ParseFile acquires a write lock then calls parseFile.
+func (w *Workspace) ParseFile(path string) error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	return w.parseFile(path)
+}
+
+// parseFile parses all YAML objects at the given path and updates the workspace
+// node cache.
+func (w *Workspace) parseFile(path string) error {
+	f, err := parser.ParseFile(path, parser.ParseComments)
+	if err != nil {
+		return err
+	}
+	for _, doc := range f.Docs {
+		// NOTE(hasheddan): currently we just skip a workspace node in the case
+		// that we cannot construct its unique identifier.
+		gvNode, err := gvPath.FilterNode(doc.Body)
+		if err != nil {
+			return err
+		}
+		if gvNode.GetToken() == nil {
+			continue
+		}
+		kNode, err := kPath.FilterNode(doc.Body)
+		if err != nil {
+			return err
+		}
+		if kNode.GetToken() == nil {
+			continue
+		}
+		nNode, err := nPath.FilterNode(doc.Body)
+		if err != nil {
+			return err
+		}
+		if nNode.GetToken() == nil {
+			continue
+		}
+		gvk := schema.FromAPIVersionAndKind(gvNode.GetToken().Value, kNode.GetToken().Value)
+		w.nodes[nodeID(nNode.GetToken().Value, gvk)] = &PackageNode{
+			ast:      doc.Body,
+			fileName: path,
+			gvk:      gvk,
+		}
+	}
+	return nil
+}
+
+// A NodeFilterFn filters the node on which we perform validation.
+type NodeFilterFn func(nodes map[NodeIdentifier]Node) []Node
+
+// AllNodes does not filter out any nodes in the workspace.
+func AllNodes(nodes map[NodeIdentifier]Node) []Node {
+	ns := make([]Node, len(nodes))
+	i := 0
+	for _, n := range nodes {
+		ns[i] = n
+		i++
+	}
+	return ns
+}
+
+// Validate performs validation on all filtered nodes and returns diagnostics
+// for any validation errors encountered.
+// TODO(hasheddan): consider decoupling forming diagnostics from getting
+// validation errors.
+func (w *Workspace) Validate(fn NodeFilterFn) ([]lsp.Diagnostic, error) {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+	diags := []lsp.Diagnostic{}
+	for _, n := range fn(w.nodes) {
+		v, ok := w.validators[n.GetGVK()]
+		if !ok {
+			return nil, errors.Errorf(errMissingValidatorFmt, n.GetGVK())
+		}
+		var node interface{}
+		if err := yaml.NewDecoder(n.GetAST()).Decode(&node); err != nil {
+			return nil, errors.Wrap(err, errParseNode)
+		}
+		for _, err := range v.Validate(node).Errors {
+			if err, ok := err.(*verrors.Validation); ok {
+				// TODO(hasheddan): handle the case where error occurs and we
+				// don't have a valid path.
+				vErr := err
+				if len(err.Name) > 0 && err.Name != "." {
+					errPath := err.Name
+					if err.Code() == verrors.RequiredFailCode {
+						errPath = err.Name[:strings.LastIndex(err.Name, ".")]
+					}
+					path, err := yaml.PathString("$." + errPath)
+					if err != nil {
+						continue
+					}
+					node, err := path.FilterNode(n.GetAST())
+					if err != nil {
+						continue
+					}
+					tok := node.GetToken()
+					if tok != nil {
+						// TODO(hasheddan): token position reflects file line
+						// and column by NOT being zero-indexed, but VSCode
+						// interprets ranges with zero-indexing. We should
+						// develop a more robust solution for this conversion.
+						diags = append(diags, lsp.Diagnostic{
+							Range: lsp.Range{
+								Start: lsp.Position{
+									Line:      tok.Position.Line - 1,
+									Character: tok.Position.Column - 1,
+								},
+								End: lsp.Position{
+									Line:      tok.Position.Line - 1,
+									Character: tok.Position.Column + len(tok.Value) - 1,
+								},
+							},
+							Message:  vErr.Error(),
+							Severity: lsp.Error,
+							Source:   serverName,
+						})
+					}
+				}
+			}
+		}
+	}
+	return diags, nil
+}
+
+// validatorsFromDir loads all validators from the specified directory.
+// TODO(hasheddan): we currently assume that the cache holds objects in their
+// CRD form, but it is more likely that we will need to extract them from
+// packages.
+func validatorsFromDir(path string) (map[schema.GroupVersionKind]*validate.SchemaValidator, error) {
+	validators := map[schema.GroupVersionKind]*validate.SchemaValidator{}
+	if err := fs.WalkDir(os.DirFS(path), ".", func(p string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		f, err := os.Open(filepath.Join(path, p))
+		defer f.Close()
+		yr := apimachyaml.NewYAMLReader(bufio.NewReader(f))
+		for {
+			b, err := yr.Read()
+			if err != nil && err != io.EOF {
+				return err
+			}
+			if err == io.EOF {
+				break
+			}
+			if len(b) == 0 {
+				continue
+			}
+			// TODO(hasheddan): handle v1beta1 CRDs, as well as all XRD API versions.
+			crd := &extv1.CustomResourceDefinition{}
+			if err := k8syaml.Unmarshal(b, crd); err != nil {
+				// Skip YAML document if we cannot unmarshal to v1 CRD.
+				continue
+			}
+			internal := &ext.CustomResourceDefinition{}
+			if err := extv1.Convert_v1_CustomResourceDefinition_To_apiextensions_CustomResourceDefinition(crd, internal, nil); err != nil {
+				return err
+			}
+			// NOTE(hasheddan): If top-level validation is set, we use it for
+			// all versions and continue.
+			if internal.Spec.Validation != nil {
+				sv, _, err := validation.NewSchemaValidator(internal.Spec.Validation)
+				if err != nil {
+					return err
+				}
+				for _, v := range internal.Spec.Versions {
+					validators[schema.GroupVersionKind{
+						Group:   internal.Spec.Group,
+						Version: v.Name,
+						Kind:    internal.Spec.Names.Kind,
+					}] = sv
+				}
+				continue
+			}
+			for _, v := range internal.Spec.Versions {
+				sv, _, err := validation.NewSchemaValidator(v.Schema)
+				if err != nil {
+					return err
+				}
+				validators[schema.GroupVersionKind{
+					Group:   internal.Spec.Group,
+					Version: v.Name,
+					Kind:    internal.Spec.Names.Kind,
+				}] = sv
+			}
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return validators, nil
+}
