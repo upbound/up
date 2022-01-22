45a48b8ecfd49ab3837dbb1ede7df110eca3d22f
Taylor Thornton <thornton.tn@gmail.com>
2022-01-21 16:04:38 -0800

    Add showMessage support to `xpls` - emits a message to the client informing them that a new version is available of up
    Signed-off-by: Taylor Thornton <thornton.tn@gmail.com>

---
+++ internal/xpls/version/version.go
@@ -0,0 +1,140 @@
+// Copyright 2022 Upbound Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package version
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/Masterminds/semver/v3"
+
+	"github.com/crossplane/crossplane-runtime/pkg/logging"
+
+	"github.com/upbound/up/internal/version"
+)
+
+const (
+	// 5 seconds should be more than enough time.
+	clientTimeout = 5 * time.Second
+	cliURL        = "https://cli.upbound.io/stable/current/version"
+
+	errFailedToQueryRemoteFmt = "query to %s failed"
+	errNotSemVer              = "couldn't covert version to semver"
+)
+
+type client interface {
+	Do(*http.Request) (*http.Response, error)
+}
+
+type defaultClient struct {
+	client http.Client
+}
+
+// Informer enables the caller to determine if they can upgrade their current
+// version of up.
+type Informer struct {
+	client client
+	log    logging.Logger
+}
+
+// NewInformer constructs a new Informer.
+func NewInformer(opts ...Option) *Informer {
+	i := &Informer{
+		log:    logging.NewNopLogger(),
+		client: newClient(),
+	}
+
+	for _, o := range opts {
+		o(i)
+	}
+
+	return i
+}
+
+// Option modifies the Informer.
+type Option func(*Informer)
+
+// WithLogger overrides the default logger for the Informer.
+func WithLogger(l logging.Logger) Option {
+	return func(i *Informer) {
+		i.log = l
+	}
+}
+
+// CanUpgrade queries locally for the version of up, uses the Informer's client
+// to check what the currently published version of up is and returns the local
+// and remote versions and whether or not we could upgrade up.
+func (i *Informer) CanUpgrade() (string, string, bool) {
+	local := version.GetVersion()
+	remote, err := i.getCurrent()
+	if err != nil {
+		i.log.Debug(fmt.Sprintf(errFailedToQueryRemoteFmt, cliURL), "error", err)
+		return "", "", false
+	}
+
+	return local, remote, i.newAvailable(local, remote)
+}
+
+func (i *Informer) newAvailable(local, remote string) bool {
+	lv, err := semver.NewVersion(local)
+	if err != nil {
+		// invalid remote version detected
+		i.log.Debug(errNotSemVer, "error", err)
+		return false
+	}
+	rv, err := semver.NewVersion(remote)
+	if err != nil {
+		// invalid remote version detected
+		i.log.Debug(errNotSemVer, "error", err)
+		return false
+	}
+
+	return rv.GreaterThan(lv)
+}
+
+func (i *Informer) getCurrent() (string, error) {
+	r, err := http.NewRequestWithContext(context.Background(), http.MethodGet, cliURL, nil)
+	if err != nil {
+		return "", err
+	}
+	resp, err := i.client.Do(r)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close() // nolint:errcheck
+
+	v, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.Trim(string(v), "\n"), nil
+}
+
+func newClient() *defaultClient {
+	return &defaultClient{
+		client: http.Client{
+			Timeout: clientTimeout,
+		},
+	}
+}
+
+func (d *defaultClient) Do(r *http.Request) (*http.Response, error) {
+	return d.client.Do(r)
+}
---
+++ internal/xpls/version/version_test.go
@@ -0,0 +1,168 @@
+// Copyright 2022 Upbound Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package version
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/crossplane/crossplane-runtime/pkg/test"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestNewAvailable(t *testing.T) {
+	type args struct {
+		i      *Informer
+		local  string
+		remote string
+	}
+
+	type want struct {
+		update bool
+	}
+
+	cases := map[string]struct {
+		reason string
+		args   args
+		want   want
+	}{
+		"NewVersionAvailable": {
+			reason: "Should return true due to new version available.",
+			args: args{
+				i:      &Informer{},
+				local:  "v0.0.1",
+				remote: "v0.1.0",
+			},
+			want: want{
+				update: true,
+			},
+		},
+		"OlderVersionAvailable": {
+			reason: "Should return false due to old version available.",
+			args: args{
+				i:      &Informer{},
+				local:  "v0.1.0",
+				remote: "v0.0.1",
+			},
+			want: want{
+				update: false,
+			},
+		},
+		"NewDevVersionLocal": {
+			reason: "Should return false due to old version available.",
+			args: args{
+				i:      &Informer{},
+				local:  "v0.7.0-rc.0.dirty",
+				remote: "v0.6.0",
+			},
+			want: want{
+				update: false,
+			},
+		},
+		"DevVersionLocalNewVersionRemote": {
+			reason: "Should return true due to new version available.",
+			args: args{
+				i:      &Informer{},
+				local:  "v0.7.0-rc.0.dirty",
+				remote: "v0.8.0",
+			},
+			want: want{
+				update: true,
+			},
+		},
+	}
+
+	for _, tc := range cases {
+
+		got := tc.args.i.newAvailable(tc.args.local, tc.args.remote)
+
+		if diff := cmp.Diff(tc.want.update, got); diff != "" {
+			t.Errorf("\n%s\nNewAvailable(...): -want err, +got err:\n%s", tc.reason, diff)
+		}
+	}
+}
+
+func TestGetCurrent(t *testing.T) {
+	type args struct {
+		i *Informer
+	}
+
+	type want struct {
+		version string
+		err     error
+	}
+
+	cases := map[string]struct {
+		reason string
+		args   args
+		want   want
+	}{
+		"SuccessfulCall": {
+			reason: "Should return expected version if successful.",
+			args: args{
+				i: &Informer{
+					client: &mockClient{
+						version: "v0.6.0",
+					},
+				},
+			},
+			want: want{
+				version: "v0.6.0",
+			},
+		},
+		"FailedCall": {
+			reason: "Should return error due to failed call",
+			args: args{
+				i: &Informer{
+					client: &mockClient{
+						err: errors.New("boom"),
+					},
+				},
+			},
+			want: want{
+				err: errors.New("boom"),
+			},
+		},
+	}
+
+	for _, tc := range cases {
+
+		version, err := tc.args.i.getCurrent()
+
+		if diff := cmp.Diff(tc.want.version, version); diff != "" {
+			t.Errorf("\n%s\nGetCurrent(...): -want err, +got err:\n%s", tc.reason, diff)
+		}
+
+		if diff := cmp.Diff(tc.want.err, err, test.EquateErrors()); diff != "" {
+			t.Errorf("\n%s\nGetCurrent(...): -want err, +got err:\n%s", tc.reason, diff)
+		}
+	}
+}
+
+type mockClient struct {
+	version string
+	err     error
+}
+
+func (m *mockClient) Do(r *http.Request) (*http.Response, error) {
+	return &http.Response{
+		// NOTE(@tnthornton) the response from the real cli.upbound.io includes a `\n`
+		Body: io.NopCloser(bytes.NewBufferString(fmt.Sprintf("%s\n", m.version))),
+	}, m.err
+}
