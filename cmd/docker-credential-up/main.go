0a06339c4e895884fa66331918e5aff1c23b3d8f
hasheddan <georgedanielmangum@gmail.com>
2022-04-20 13:57:27 -0400

    Add new docker-credential-up binary
    Adds a new binary for docker-credential-up, which supplies credentials
    to OCI clients in accordance with the docker credential helper protocol.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

---
+++ cmd/docker-credential-up/main.go
@@ -0,0 +1,43 @@
+// Copyright 2022 Upbound Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/docker/docker-credential-helpers/credentials"
+
+	"github.com/upbound/up/internal/credhelper"
+	"github.com/upbound/up/internal/version"
+)
+
+const profile = "UP_PROFILE"
+
+func main() {
+	var v bool
+	flag.BoolVar(&v, "v", false, "Print CLI version and exit.")
+	flag.Parse()
+
+	if v {
+		fmt.Fprintln(os.Stdout, version.GetVersion())
+		os.Exit(0)
+	}
+
+	// Build credential helper and defer execution to Docker.
+	h := credhelper.New(credhelper.WithProfile(os.Getenv(profile)))
+	credentials.Serve(h)
+}
