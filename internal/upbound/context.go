4301df9469a221555bf04967adaa499adabaaf17
hasheddan <georgedanielmangum@gmail.com>
2022-05-24 15:20:16 -0400

    Move flags and context building to common package
    Moves all commnon flags and context required to communicate with Upbound
    to a common package to be consumed by relevant commands.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- internal/upbound/context.go
+++ internal/upbound/context.go
@@ -29,40 +29,109 @@ const (
 	UserAgent = "up-cli"
 	// CookieName is the default cookie name used to identify a session token.
 	CookieName = "SID"
+
+	// Default API subdomain.
+	apiSubdomain = "api."
+	// Default registry subdomain.
+	xpkgSubdomain = "xpkg."
 )
+
+// Flags are common flags used by commands that interact with Upbound.
+type Flags struct {
+	// Optional
+	Domain  *url.URL `env:"UP_DOMAIN" default:"https://upbound.io" help:"Root Upbound domain."`
+	Profile string   `env:"UP_PROFILE" help:"Profile used to execute command."`
+	Account string   `short:"a" env:"UP_ACCOUNT" help:"Account used to execute command."`
+
+	APIEndpoint      *url.URL `env:"OVERRIDE_API_ENDPOINT" hidden:"" name:"override-api-endpoint" help:"Overrides the default API endpoint."`
+	RegistryEndpoint *url.URL `env:"OVERRIDE_REGISTRY_ENDPOINT" hidden:"" name:"override-registry-endpoint" help:"Overrides the default registry endpoint."`
+}
 
 // Context includes common data that Upbound consumers may utilize.
 type Context struct {
-	Profile  string
-	ID       string
-	Token    string
-	Type     config.ProfileType
-	Account  string
-	Endpoint *url.URL
-	Cfg      *config.Config
-	CfgSrc   config.Source
+	Profile          config.Profile
+	Token            string
+	Account          string
+	Domain           *url.URL
+	APIEndpoint      *url.URL
+	RegistryEndpoint *url.URL
+	Cfg              *config.Config
+	CfgSrc           config.Source
+}
+
+// NewFromFlags constructs a new context from flags.
+func NewFromFlags(f Flags) (*Context, error) {
+	src := config.NewFSSource()
+	if err := src.Initialize(); err != nil {
+		return nil, err
+	}
+	conf, err := config.Extract(src)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Context{
+		Account: f.Account,
+		Domain:  f.Domain,
+		Cfg:     conf,
+		CfgSrc:  src,
+	}
+
+	c.APIEndpoint = f.APIEndpoint
+	if c.APIEndpoint == nil {
+		u := *c.Domain
+		u.Host = apiSubdomain + u.Host
+		c.APIEndpoint = &u
+	}
+
+	c.RegistryEndpoint = f.RegistryEndpoint
+	if c.RegistryEndpoint == nil {
+		u := *c.Domain
+		u.Host = xpkgSubdomain + u.Host
+		c.RegistryEndpoint = &u
+	}
+
+	// If profile identifier is not provided, use the default, or empty if the
+	// default cannot be obtained.
+	c.Profile = config.Profile{}
+	if f.Profile == "" {
+		if _, p, err := c.Cfg.GetDefaultUpboundProfile(); err == nil {
+			c.Profile = p
+		}
+	} else {
+		if p, err := c.Cfg.GetUpboundProfile(f.Profile); err == nil {
+			c.Profile = p
+		}
+	}
+
+	// If account has not already been set, use the profile default.
+	if c.Account == "" {
+		c.Account = c.Profile.Account
+	}
+
+	return c, nil
 }
 
 // BuildSDKConfig builds an Upbound SDK config suitable for usage with any
 // service client.
-func BuildSDKConfig(session string, endpoint *url.URL) (*up.Config, error) {
+func (c *Context) BuildSDKConfig(session string) (*up.Config, error) {
 	cj, err := cookiejar.New(nil)
 	if err != nil {
 		return nil, err
 	}
-	cj.SetCookies(endpoint, []*http.Cookie{{
+	cj.SetCookies(c.APIEndpoint, []*http.Cookie{{
 		Name:  CookieName,
 		Value: session,
 	},
 	})
-	client := up.NewClient(func(c *up.HTTPClient) {
-		c.BaseURL = endpoint
-		c.HTTP = &http.Client{
+	client := up.NewClient(func(u *up.HTTPClient) {
+		u.BaseURL = c.APIEndpoint
+		u.HTTP = &http.Client{
 			Jar: cj,
 		}
-		c.UserAgent = UserAgent
+		u.UserAgent = UserAgent
 	})
-	return up.NewConfig(func(c *up.Config) {
-		c.Client = client
+	return up.NewConfig(func(conf *up.Config) {
+		conf.Client = client
 	}), nil
 }
