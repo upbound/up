8d75a7477e62156bb2007f1d371eb7093e86e658
nullable-eth <2248325+nullable-eth@users.noreply.github.com>
2023-07-05 09:38:02 -0400

    ConnectionDetail removed from v1alpha1
    https: //github.com/crossplane/crossplane/commit/8fbff7d4dbf458ae92451896886cf762b2f466c6
    Signed-off-by: nullable-eth <2248325+nullable-eth@users.noreply.github.com>

--- internal/vendor/github.com/crossplane/crossplane/controller/apiextensions/composite/revision.go
+++ internal/vendor/github.com/crossplane/crossplane/controller/apiextensions/composite/revision.go
@@ -18,12 +18,11 @@ package composite
 
 import (
 	v1 "github.com/crossplane/crossplane/apis/apiextensions/v1"
-	"github.com/crossplane/crossplane/apis/apiextensions/v1alpha1"
 )
 
 // AsCompositionConnectionDetail translates a composition revision's connection
 // detail to a composition connection detail.
-func AsCompositionConnectionDetail(rcd v1alpha1.ConnectionDetail) v1.ConnectionDetail {
+func AsCompositionConnectionDetail(rcd v1.ConnectionDetail) v1.ConnectionDetail {
 	return v1.ConnectionDetail{
 		Name: rcd.Name,
 		Type: func() *v1.ConnectionDetailType {
@@ -41,7 +40,7 @@ func AsCompositionConnectionDetail(rcd v1alpha1.ConnectionDetail) v1.ConnectionD
 
 // AsCompositionReadinessCheck translates a composition revision's readiness
 // check to a composition readiness check.
-func AsCompositionReadinessCheck(rrc v1alpha1.ReadinessCheck) v1.ReadinessCheck {
+func AsCompositionReadinessCheck(rrc v1.ReadinessCheck) v1.ReadinessCheck {
 	return v1.ReadinessCheck{
 		Type:         v1.ReadinessCheckType(rrc.Type),
 		FieldPath:    rrc.FieldPath,
