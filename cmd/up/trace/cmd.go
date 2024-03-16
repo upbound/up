3bebb2ef87a7705ef4e19568d191883d86e2a6f7
Dr. Stefan Schimanski <stefan.schimanski@upbound.com>
2024-03-16 16:20:04 +0100

    trace/query/get: add paging
    Signed-off-by: Dr. Stefan Schimanski <stefan.schimanski@upbound.com>

--- cmd/up/trace/cmd.go
+++ cmd/up/trace/cmd.go
@@ -74,7 +74,7 @@ func (c *Cmd) AfterApply(kongCtx *kong.Context) error {
 	return nil
 }
 
-func (c *Cmd) Run(ctx context.Context, upCtx *upbound.Context) error { // nolint:gocyclo // TODO: split up
+func (c *Cmd) Run(ctx context.Context, kongCtx *kong.Context, upCtx *upbound.Context) error { // nolint:gocyclo // TODO: split up
 	// create client
 	kubeconfig, ns, err := upCtx.Profile.GetSpaceKubeConfig()
 	if err != nil {
@@ -148,11 +148,26 @@ func (c *Cmd) Run(ctx context.Context, upCtx *upbound.Context) error { // nolint
 
 		var objs []queryv1alpha1.QueryResponseObject
 		for _, spec := range querySpecs {
-			query := queryObject.DeepCopyQueryObject().SetSpec(spec)
-			if err := kc.Create(ctx, query); err != nil {
-				return nil, fmt.Errorf("%T request failed: %w", query, err)
+			var cursor string
+			var page int
+			for {
+				spec := spec.DeepCopy()
+				spec.QueryTopLevelResources.QueryResources.Page.Cursor = cursor
+				query := queryObject.DeepCopyQueryObject().SetSpec(spec)
+
+				if err := kc.Create(ctx, query); err != nil {
+					return nil, fmt.Errorf("%T request failed: %w", query, err)
+				}
+				resp := query.GetResponse()
+				objs = append(objs, resp.Objects...)
+
+				// do paging
+				cursor = resp.Cursor.Next
+				page++
+				if cursor == "" {
+					break
+				}
 			}
-			objs = append(objs, query.GetResponse().Objects...)
 		}
 
 		return objs, nil
@@ -204,6 +219,8 @@ func createQuerySpec(obj types.NamespacedName, gk metav1.GroupKind, categories [
 				},
 			},
 			QueryResources: queryv1alpha1.QueryResources{
+				Limit:  500,
+				Cursor: true,
 				Objects: &queryv1alpha1.QueryObjects{
 					ID:           true,
 					ControlPlane: true,
