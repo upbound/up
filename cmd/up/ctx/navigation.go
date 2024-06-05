23b02fddc1f58e2ee1e1675b332b8c4521fb25b5
Adam Wolfe Gordon <awg@upbound.io>
2024-06-05 15:30:45 -0600

    ctx: Improve wording for empty groups
    Signed-off-by: Adam Wolfe Gordon <awg@upbound.io>

--- cmd/up/ctx/navigation.go
+++ cmd/up/ctx/navigation.go
@@ -545,7 +545,7 @@ func (g *Group) Items(ctx context.Context, upCtx *upbound.Context, navCtx *navCo
 	}
 
 	if len(ctps.Items) == 0 {
-		items = append(items, item{text: fmt.Sprintf("No control planes found in the %q group", g.Name), notSelectable: true})
+		items = append(items, item{text: fmt.Sprintf("No control planes found in group %q", g.Name), notSelectable: true})
 	}
 
 	items = append(items, item{text: fmt.Sprintf("Switch context to %q", fmt.Sprintf("%s/%s", g.Space.Name, g.Name)), onEnter: func(m model) (model, error) {
