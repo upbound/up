b78b2b63b0abb497ca399cbc131395f5ce99cd9f
Christopher Haar <christopher.haar@upbound.io>
2024-08-23 17:04:06 +0200

    feat(space-mirror): add resources based on kubeVersionPath
    Signed-off-by: Christopher Haar <christopher.haar@upbound.io>

--- cmd/up/space/mirror/static_artifacts.go
+++ cmd/up/space/mirror/static_artifacts.go
@@ -16,7 +16,10 @@ package mirror
 
 import (
 	_ "embed"
+	"encoding/json"
 	"reflect"
+
+	"github.com/crossplane/crossplane-runtime/pkg/errors"
 )
 
 // Embed the YAML file.
@@ -32,11 +35,48 @@ type UXPVersionsPath struct {
 	} `json:"controller"`
 }
 
+type KubeVersionPath struct {
+	ControlPlanes struct {
+		K8sVersion StringOrArray `json:"k8sVersion"`
+	} `json:"controlPlanes"`
+}
+
 func (j *UXPVersionsPath) GetSupportedVersions() ([]string, error) {
+	if len(j.Controller.Crossplane.SupportedVersions) == 0 {
+		return nil, errors.New("no supported versions found in UXPVersionsPath")
+	}
 	return j.Controller.Crossplane.SupportedVersions, nil
+}
+
+func (k *KubeVersionPath) GetSupportedVersions() ([]string, error) {
+	if len(k.ControlPlanes.K8sVersion) == 0 {
+		return nil, errors.New("no supported versions found in KubeVersionPath")
+	}
+	return k.ControlPlanes.K8sVersion, nil
 }
 
 // init function to return byte slice and oci.PathNavigator
 func initConfig() ([]byte, map[string]reflect.Type) {
-	return configFile, map[string]reflect.Type{"uxpVersionsPath": reflect.TypeOf(UXPVersionsPath{})}
+	return configFile, map[string]reflect.Type{
+		"uxpVersionsPath": reflect.TypeOf(UXPVersionsPath{}),
+		"kubeVersionPath": reflect.TypeOf(KubeVersionPath{}),
+	}
+}
+
+type StringOrArray []string
+
+func (s *StringOrArray) UnmarshalJSON(data []byte) error {
+	var single string
+	if err := json.Unmarshal(data, &single); err == nil {
+		*s = []string{single}
+		return nil
+	}
+
+	var array []string
+	if err := json.Unmarshal(data, &array); err == nil {
+		*s = array
+		return nil
+	}
+
+	return errors.New("data is neither a string nor an array of strings")
 }
